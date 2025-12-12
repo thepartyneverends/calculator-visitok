83fcea070d0d7e8cf53964e3f97c36f8842f1447
thepartyneverends <153498661+thepartyneverends@users.noreply.github.com>
2025-12-12 11:22:51 +0300

    Update my.js

--- my.js
+++ my.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function(){
     let tirSize,
     pricePaper = 0,
     priceForma = 0,
@@ -19,52 +19,43 @@
         "Цветная с лицевой, ч/б с оборотной": {file:220, print: 51},
         "Двусторонняя цветная": {file:220, print: 74},
     }
-    let select_papper=document.getElementById("card-paper");
-    let select_color=document.getElementById("card-color");
-    let input_count=document.getElementById("card-quantity");
-    let span_sum=document.getElementById("final_price");
     insertPaper();
     function insertPaper(){
         let html="";
         for (type in typePaper)
             html += '<option value="' + type + '">' + type + "</option>";
-       select_papper.innerHTML=html;
+        $("#card-paper").append(html);
     }
-  insertColor();
+    insertColor();
     function insertColor(){
         let html="";
         for (color in typeColor)
             html += '<option value="' + color + '">' + color + "</option>";
-      select_color.innerHTML=html;
-    
+        $("#card-color").append(html);
     }
-    let calk=document.querySelector(".calk");
-    calk.onchange=calculate;
-    function calculate(){
-        tirSize=input_count.value/30;
-     
-        pricePaper = typePaper[select_papper.value] * tirSize;
-        //alert(pricePaper);
+
+    $(".calk").change(function(){
+        tirSize=$("#card-quantity").val()/30;
+        // alert(tirSize);
+        pricePaper = typePaper[$("#card-paper").val()] * tirSize;
         old_color_format=color_format;
-        color_format = select_color.value;
+        color_format = $("#card-color").val();
         priceForma = typeColor[color_format]["file"];
         pricePrint = typeColor[color_format]["print"] * tirSize;
         sum=pricePaper +  priceForma + pricePrint;
-        span_sum.innerHTML=sum;
+        $("#final_price").text(sum);
         if(color_format!=old_color_format){
-        document.querySelectorAll("img").forEach(function(image){
-            image.style.display="none";
-        });
+        $("img").hide();
         if(color_format=="Односторонняя черно-белая")
-        document.querySelector("#card_1_0").style.display="block";
+        $("#card_1_0").show("slow");
         if(color_format=="Двусторонняя черно-белая")
-        document.querySelector("#card_1_1").style.display="block";
+        $("#card_1_1").show("slow");
         if(color_format=="Односторонняя цветная")
-        document.querySelector("#card_4_0").style.display="block";
+        $("#card_4_0").show("slow");
         if(color_format=="Цветная с лицевой, ч/б с оборотной")
-        document.querySelector("#card_4_1").style.display="block";
+        $("#card_4_1").show("slow");
         if(color_format=="Двусторонняя цветная")
-        document.querySelector("#card_4_4").style.display="block";
+        $("#card_4_4").show("slow");
     }
-
-}
+    });
+});
